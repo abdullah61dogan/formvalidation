071d4c802ec93e7b99d1459627bfa127947d2ece
Abdullah Doğan <abdullah.dogan@codeck.com.tr>
2022-04-18 06:47:22 +0300

    class based view

--- feedvack/reviews/views.py
+++ feedvack/reviews/views.py
@@ -1,22 +1,33 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import redirect, render
+from django.shortcuts import  render
 from .forms import ReviewForm
+from django.views import View
+
 # Create your views here.
 
-def review(request):
-    if request.method == 'POST':
+class ReviewView(View):
+    def get(self, request):
+      form = ReviewForm()
+
+      return render(request, "reviews/review.html",{
+        "form": form
+    })
+
+    def post(self, request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
+            form.save()
             return HttpResponseRedirect("/thank-you")
 
-    else:
-      form = ReviewForm()
+        return render(request, "reviews/review.html",{
+            "form": form
+        })
 
-    return render(request, "reviews/review.html",{
-      "form": form
-    })
+
+
+
+
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
